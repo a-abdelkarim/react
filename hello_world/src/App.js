8f8f5683791b164f441e85b8b6df496ab7a5be41
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-27 15:59:33 +0300

    dynamic  change user name

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(userObject) {
+    return `${userObject.firstName} ${userObject.lastName}`
+}
+
 function App() {
-  const name = "Ahmed"
-  return (
-    <div className="App">
-      <h1>Hello {name}</h1>
-    </div>
-  );
+    const user = {
+        firstName: "Ahmed",
+        lastName: "Abdelkarim",
+    }
+    return (
+        <div className="App">
+        <h1>Hello {formatName(user)}</h1>
+        </div>
+    );
 }
 
 export default App;
