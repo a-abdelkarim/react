cc22f1089516097dd26c12210da1ccd4e82df758
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-09-30 13:15:59 +0300

    convert function component to class component

--- hello_world/src/components/Products.js
+++ hello_world/src/components/Products.js
@@ -1,21 +1,34 @@
+import React from "react";
 import ProductTitle from "./ProductTitle";
 
-export default function Products(props)
+
+export default class Products extends React.PureComponent
 {
-    return (
-        <>
-            <div>
-                <ProductTitle title="title1" />
-                <h6>Description</h6>
-            </div>
-            <div>
-                <ProductTitle title="title2" />
-                <h6>Description</h6>
-            </div>
-            <div>
-                <ProductTitle title="title3" />
-                <h6>Description</h6>
-            </div>
-        </>
-    )
+    constructor(props) 
+    {
+        super(props);
+        this.state = {
+        
+        }
+    }
+
+    render()
+    {
+        return (
+            <>
+                <div>
+                    <ProductTitle title="title1" />
+                    <h6>Description</h6>
+                </div>
+                <div>
+                    <ProductTitle title="title2" />
+                    <h6>Description</h6>
+                </div>
+                <div>
+                    <ProductTitle title="title3" />
+                    <h6>Description</h6>
+                </div>
+            </>
+        )
+    }
 }
